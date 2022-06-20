c2f9ce904196ef07284c62980b5a990f017de89e
Maciej Tomala <Maciejtomala1993@gmail.com>
2022-06-21 01:27:16 +0200

    added test data and swagger

---
+++ Flights/ReadModels/FlightRm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.ReadModels
+{
+    public record FlightRm(
+        Guid Id, 
+        string Airline,
+        string Price,
+        TimePlaceRm Departure,
+        TimePlaceRm Arrival,
+        int RemainingNumberOfSeats
+
+        );
+}
---
+++ Flights/ReadModels/TimePlaceRm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights.ReadModels
+{
+    public record TimePlaceRm(string Place, DateTime Time);
+  
+}
